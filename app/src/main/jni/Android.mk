829ee5b60a27b989fc8148e4a9a2310345614c58
LGLTeam <61142621+LGLTeam@users.noreply.github.com>
2020-03-24 21:15:24 +0100

    Add files via upload

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,15 +2,19 @@ LOCAL_PATH := $(call my-dir)
 MAIN_LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_ARM_MODE := arm
-
+# Here is the name of your lib.
+# When you change the lib name, change also on System.loadLibrary("") under OnCreate method on FloatingModMenuService.java
+# Both must have same name
 LOCAL_MODULE    := LGLTeam
 
+# Stuff you don't need to know
+LOCAL_ARM_MODE := arm
 LOCAL_CFLAGS := -Wno-error=format-security -fpermissive -fvisibility=hidden -fvisibility-inlines-hidden
 LOCAL_CFLAGS += -fno-rtti -fno-exceptions -g0 -fomit-frame-pointer -ffunction-sections -fdata-sections
 LOCAL_CPPFLAGS += -fvisibility=hidden -ffunction-sections -fdata-sections
 LOCAL_LDFLAGS += -Wl,--strip-all
 
+# Here you add the cpp file
 LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)
 LOCAL_SRC_FILES := src/main.cpp \
 	src/Substrate/hde64.c \

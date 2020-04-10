753e86cfc0cfbc1d664fd10276c8b043e1558c59
LGLTeam <61142621+LGLTeam@users.noreply.github.com>
2020-04-10 21:54:58 +0200

    Add files via upload

--- app/src/main/jni/src/Includes/Logger.h
+++ app/src/main/jni/src/Includes/Logger.h
@@ -10,7 +10,7 @@ enum daLogType {
 };
 
 //Change this to another Log Tag if ya want. IN the batch script I provide you change the log tag then too
-#define TAG "DarkDawn"
+#define TAG "Mod Menu"
 
 #define LOGD(...) ((void)__android_log_print(daDEBUG, TAG, __VA_ARGS__))
 #define LOGE(...) ((void)__android_log_print(daERROR, TAG, __VA_ARGS__))

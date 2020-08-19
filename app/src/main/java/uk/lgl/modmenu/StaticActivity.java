8fe38718466cfcbe86b459ec09401b0283889957
LGLTeam <61142621+LGLTeam@users.noreply.github.com>
2020-08-19 11:29:58 +0200

    Update StaticActivity.java

--- app/src/main/java/uk/lgl/modmenu/StaticActivity.java
+++ app/src/main/java/uk/lgl/modmenu/StaticActivity.java
@@ -1,10 +1,9 @@
 package uk.lgl.modmenu;
 
-import android.content.ActivityNotFoundException;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
@@ -12,7 +11,6 @@ import android.os.Process;
 import android.provider.Settings;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -23,11 +21,27 @@ public class StaticActivity {
     public static String cacheDir;
 
     public static void Start(final Context context) {
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(context)) {
-            context.startActivity(new Intent("android.settings.action.MANAGE_OVERLAY_PERMISSION",
-                    Uri.parse("package:" + context.getPackageName())));
-            Toast.makeText(context.getApplicationContext(), "Please allow overlay permission in order to display the mod menu", Toast.LENGTH_LONG).show();
-            //Process.killProcess(Process.myPid());
+            AlertDialog alertDialog = new AlertDialog.Builder(context, 1)
+                    .setTitle("No overlay permission")
+                    .setMessage("Overlay permission is required in order to display the mod menu on top of the screen. Do you want to open the settings?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            context.startActivity(new Intent("android.settings.action.MANAGE_OVERLAY_PERMISSION",
+                                    Uri.parse("package:" + context.getPackageName())));
+                            Process.killProcess(Process.myPid());
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    }).setCancelable(false)
+                    .create();
+            alertDialog.show();
         } else {
             // When you change the lib name, change also on Android.mk file
             // Both must have same name

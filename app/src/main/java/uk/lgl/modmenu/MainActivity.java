c65519705d61ba208cf2d6fab34b432d8f0f11d6
LGLTeam <61142621+LGLTeam@users.noreply.github.com>
2020-08-22 16:28:07 +0200

    Update MainActivity.java

--- app/src/main/java/uk/lgl/modmenu/MainActivity.java
+++ app/src/main/java/uk/lgl/modmenu/MainActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 
 import uk.lgl.loadlib.LoadLib;
 
+
 public class MainActivity extends Activity {
+    boolean Switch = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         StaticActivity.Start(this);
-        //LoadLib.Start(this);
+
+        if (Switch){ //keep this code to force compile it. If it's unused, it won't compile LoadLib file
+            LoadLib.Start(this);
+        }
     }
 }

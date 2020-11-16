c2fd8d0bfe9bb9b50fe099cd762f39a8f436009e
Yuki.kaco <35301327+asd9176506911298@users.noreply.github.com>
2020-11-16 18:42:37 +0800

    Update MainActivity.java

--- app/src/main/java/uk/lgl/MainActivity.java
+++ app/src/main/java/uk/lgl/MainActivity.java
@@ -3,6 +3,7 @@ package uk.lgl;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
